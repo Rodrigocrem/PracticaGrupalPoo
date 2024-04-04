28a35f9d9e26c7800bd1fc1212baf3ef25edbeae
Rodrigo López <rodrigolopch@gmail.com>
2024-04-04 10:24:22 +0200

    No funciona Test

---
+++ MquinaDeVending/MquinaDeVending/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MquinaDeVending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos a nuestra maquina de vending :)");
+            Console.ReadKey();
+        }
+    }
+}
--- Program.cs
+++
@@ -1,1 +0,0 @@
-##Aqui pondremos la clase program
