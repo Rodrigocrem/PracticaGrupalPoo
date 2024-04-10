18b7b5a8003211b2b2ff32d8c92474d187c977f2
Rodrigo López <rodrigolopch@gmail.com>
2024-04-10 19:56:25 +0200

    Cositas

--- MquinaDeVending/MquinaDeVending/Program.cs
+++ MquinaDeVending/MquinaDeVending/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -11,26 +12,24 @@ namespace MquinaDeVending
     internal class Program
     {
         static List<Product_General> listaproductos = new List<Product_General>();
+
         static void Main(string[] args)
         {
             int opcion = 0;
             do
             {
-                Console.WriteLine("...................................................");
-                Console.WriteLine(".     Bienvenidos a nuestra maquina de vending     .");
-                Console.WriteLine("...................................................");
-                Console.WriteLine(".      Para comenzar seleccione una opcion:       .");
-                Console.WriteLine("...................................................");
-                Console.WriteLine(".      1. Comprar un producto                     .");
-                Console.WriteLine("...................................................");
-                Console.WriteLine("       2. Mostrar informacion de un producto      .");
-                Console.WriteLine("...................................................");
-                Console.WriteLine(".      3. Cargar productos de manera individual   .");
-                Console.WriteLine("...................................................");
-                Console.WriteLine(".      4. Cargar productos a tope                 .");
-                Console.WriteLine("...................................................");
-                Console.WriteLine(".      5. Salir                                   .");
-                Console.WriteLine("...................................................");
+              
+
+                Console.WriteLine(".      Para comenzar seleccione una opcion:.");
+                Console.WriteLine("╔════════════════════════════════════════════╗");
+                Console.WriteLine("║            Máquina expendedora             ║");
+                Console.WriteLine("╚════════════════════════════════════════════╝");
+                Console.WriteLine("║  1 | Comprar un producto                   ║");
+                Console.WriteLine("║  2 | Mostrar informacion de un producto    ║");
+                Console.WriteLine("║  3 | Cargar productos de manera individual ║");
+                Console.WriteLine("║  4 | Cargar productos a tope               ║");
+                Console.WriteLine("║  5 | Salir                                 ║");
+                Console.WriteLine("╚════════════════════════════════════════════╝");
                 opcion = int.Parse(Console.ReadLine());
                 Console.Clear();
                 switch(opcion)
