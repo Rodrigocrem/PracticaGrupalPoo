b86900cd392394f5ca604cb95eee1c0864d76ad9
Rodrigo López <rodrigolopch@gmail.com>
2024-04-14 16:54:52 +0200

    qq

--- MquinaDeVending/MquinaDeVending/Pago_Efectivo.cs
+++ MquinaDeVending/MquinaDeVending/Pago_Efectivo.cs
@@ -11,7 +11,17 @@ namespace MquinaDeVending
         public override void RealizarPago(double cantidad)
         {
             Console.WriteLine($"Pagando en efectivo: ${cantidad}");
-            // Aquí pondremos la lógica para realizar el pago en efectivo
+            string dinero = GestionarPago(cantidad);
+            Console.WriteLine("Monedas introducidas: " + dinero);
+
+        }
+
+        private string GestionarPago(double cantidad)
+        {
+            int[] ValorDinero = { 20, 10, 5, 2, 1};
+            int[] centimos = (int)(cantidad * 100); //Revisar esto. 
+            StringBuilder monedasEntregadas = new StringBuilder();
+
         }
     }
 }
