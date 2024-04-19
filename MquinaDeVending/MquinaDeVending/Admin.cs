bcbaa49fab71c2823cb1645f204bd9d36aadad36
Rodrigo López <rodrigolopch@gmail.com>
2024-04-19 12:08:53 +0200

    aa

--- MquinaDeVending/MquinaDeVending/Admin.cs
+++ MquinaDeVending/MquinaDeVending/Admin.cs
@@ -8,5 +8,25 @@ namespace MquinaDeVending
 {
     internal class Admin
     {
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+
+        public Admin( string nickname, string password)
+        {
+            Nickname = nickname;
+            Password = password;
+        }
+
+        public bool Login(string nickname, string password) //Login(string nickname, string password), que devolverá true si el usuario y contraseña es correcto, false en caso contrario
+        {
+            if (Nickname == nickname && Password == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
