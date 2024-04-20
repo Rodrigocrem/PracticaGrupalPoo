cc4abe50556ff16201cea168507cc3ea00bcde48
Manuel Luna Jiménez <9200982@alumnos.ufv.es>
2024-04-20 16:32:11 +0200

    k

--- MquinaDeVending/MquinaDeVending/Program.cs
+++ MquinaDeVending/MquinaDeVending/Program.cs
@@ -45,13 +45,13 @@ namespace MquinaDeVending
                         //
                         break;
                     case 3:
-                        //
+                        
                         break;
                     case 4:
-                        //
+                        IniciarSesion();
                         break;
                     case 5:
-                        //
+                        IniciarSesion();
                         break;
                     case 6://Salir.
                         Console.WriteLine("Saliendo...");
@@ -169,35 +169,7 @@ namespace MquinaDeVending
                 Console.WriteLine("Usuario o contraseña incorrectos.");
             }
         }
-        public static void Registrarse()
-        {
-            Console.WriteLine("Introduce tu nombre: ");
-            string nombre = Console.ReadLine();
-            Console.WriteLine("Introduce tu primer apellido: ");
-            string ape1 = Console.ReadLine();
-            Console.WriteLine("Introduce tu segundo apellido: ");
-            string ape2 = Console.ReadLine();
-            Console.WriteLine("Introduce tu usuario: ");
-            string nickname = Console.ReadLine();
-            bool usuarioEncontrado = false;
-            foreach (Usuarios usuario2 in listaUsuarios) // Preguntar a Iván. 
-            {
-                if (usuario2.Nickname == nickname)
-                {
-                    Console.WriteLine("El usuario ya existe.");
-                    return;
-                }
-            }
-            if (usuarioEncontrado)
-            {
-
-            }
-            Console.WriteLine("Introduce tu contraseña: ");
-            string password = Console.ReadLine();
-            Cliente usuario = new Cliente(listaUsuarios.Count + 1, nickname, nombre, password, listaproductos);
-            listaUsuarios.Add(usuario);
-            usuario.ToFile(); //Guardamos el admin en el archivo.
-        }
+        
 
 
     }
